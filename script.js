1869150aad9c7a11b98cd58ec76dc21e9dcbc1ed
amir9o9 <149272606+amir9o9@users.noreply.github.com>
2025-04-05 00:53:38 +0330

    write scripts cleaner

--- script.js
+++ script.js
@@ -1,18 +1,56 @@
 //used to find current year , month and day
 const date = new Date();
+    document.getElementById("theme_check").addEventListener('change',function(){
+    document.querySelector('body').classList.toggle('body-dark-theme');
+    document.querySelector('.wraper').classList.toggle('wraper-dark-theme');
+    document.querySelector('#day').classList.toggle('input-dark-theme');
+    document.querySelector('#month').classList.toggle('input-dark-theme');
+    document.querySelector('#year').classList.toggle('input-dark-theme');
+    document.querySelector('#about_us').classList.toggle('about-us-dark-theme');
+    
+    if(document.querySelector('body').classList.contains('body-dark-theme')){
+        document.querySelector('#sun_icon').style.display="none";
+        document.querySelector('#moon_icon').style.display="block"; 
+    }else{
+        document.querySelector('#moon_icon').style.display="none";
+        document.querySelector('#sun_icon').style.display="block";
+    }   
+})
+
+function showError(elementId, labelId) {
+    document.getElementById(elementId).style.border = "1px solid red";
+    document.getElementById(labelId).style.color = "red";
+    document.getElementById(`${elementId}Error`).style.display = "block";
+}
+
+function hideError(elementId, labelId) {
+    document.getElementById(elementId).style.border = "solid 1px hsl(0, 1%, 44%)";
+    document.getElementById(labelId).style.color = "hsl(0, 1%, 44%)";
+    document.getElementById(`${elementId}Error`).style.display = "none";
+}
+
+document.addEventListener('change', function(event){ 
+    if(event.target.id === 'close-checkbox' ||  event.target.id === 'about_us' ){
+        document.getElementById("about_us").style.display ="none";
+        document.querySelector(".about_us_btn_lb").style.display ="block";
+        document.querySelector(".wraper").classList.toggle("wraper-blur");
+        document.querySelector(".wraper").style.pointerEvents="auto";
+    }
+})
+
+document.getElementById('about_us_btn').addEventListener('change', function(){
+    document.getElementById("about_us").style.display ="block";
+    document.querySelector(".about_us_btn_lb").style.display ="none";
+    document.querySelector(".wraper").classList.toggle("wraper-blur");
+    document.querySelector(".wraper").style.pointerEvents="none";
+})
 
 document.getElementById('inputForm').addEventListener('submit', function(event) {
-
-    // جلوگیری از ارسال فرم
     event.preventDefault();
-
-    // دریافت مقادیر ورودی
     let day = parseInt(document.getElementById('day').value);
     let month = parseInt(document.getElementById('month').value);
     let year = parseInt(document.getElementById('year').value);
 
-    
-    // نمایش مقادیر در div
     const dayout=document.getElementById("dayOut");
     dayout.innerHTML=dayCalculator(day);  
 
@@ -26,92 +64,73 @@ document.getElementById('inputForm').addEventListener('submit', function(event) 
 //Calculate year age
 function yearCalculator(year){
 
-    let currnetYear = date.getFullYear();
+    let currentYear = date.getFullYear();
     let birthMonth=parseInt(month.value);
-    let currnetMonth=date.getMonth();
+    let currentMonth=date.getMonth();
     let result;
 
-    if(year > currnetYear){
-        document.getElementById("yearError").style.display="block";
-        document.getElementById("year").style.border="solid 1px red";
-        document.getElementById("yearLb").style.color="red";
+    if(year > currentYear){
+        showError("year", "yearLb")
         return "--";
     }
-
-    else {
-        document.getElementById("yearError").style.display="none";
-        document.getElementById("year").style.border="solid 1px hsl(0, 1%, 44%)";
-        document.getElementById("yearLb").style.color=" hsl(0, 1%, 44%)";        
-
-        //if birth month be less than currnet month ,year must be minus 1 and 12 must added to birth month
-        if(currnetMonth+1 < birthMonth){
-            result = (currnetYear-year-1);    
-        }
-
-        else{
-            result = currnetYear-year;
+    else {     
+        hideError("year", "yearLb")
+        //if birth month be less than current month ,year must be minus 1 and 12 must added to birth month
+        if(currentMonth+1 < birthMonth){
+            result = (currentYear-year-1);    
+        }else{
+            result = currentYear-year;
         }
         return result;
     }
 }
-
 
 //Calculate month of age
 function monthCalculator(month){
 
     //In JavaScript months starts 0 and end in 11(so we +1)
-    let currnetMonth = date.getMonth()+1;
-    let currnetDay = date.getDate();
+    let currentMonth = date.getMonth()+1;
+    let currentDay = date.getDate();
     let birthDay = parseInt(day.value);
     let result;
 
-    //If current day be less than birthday , month minus by 1 (and 30/31 added to currnet Day)
-    if(currnetDay < birthDay){
-        currnetMonth--; 
+    //If current day be less than birthday , month minus by 1 (and 30/31 added to current Day)
+    if(currentDay < birthDay){
+        currentMonth--; 
     }
 
     //Invalid year error
     if(month<1 || month>12){
-        document.getElementById("monthError").style.display="block";
-        document.getElementById("month").style.border="1px solid red";
-        document.getElementById("monthLb").style.color="red";
+        showError("month","monthLb");
         return"--";
     }
 
-    //If currnet Month be less than birth month 12 must added to currnet month , then minus by birth month (and year minus by 1)
-    else if(month > currnetMonth){
-        currnetMonth += 12;
-        document.getElementById("monthError").style.display="none";
-        document.getElementById("month").style.border="solid 1px hsl(0, 1%, 44%)";
-        document.getElementById("monthLb").style.color=" hsl(0, 1%, 44%)";
-        result = currnetMonth-month;
-        
+    //If current Month be less than birth month 12 must added to current month , then minus by birth month (and year minus by 1)
+    else if(month > currentMonth){
+        currentMonth += 12;
+        hideError("month","monthLb")
+        result = currentMonth-month;
         return result;
     }
 
     else {
-        document.getElementById("monthError").style.display="none";
-        document.getElementById("month").style.border="solid 1px hsl(0, 1%, 44%)";
-        document.getElementById("monthLb").style.color=" hsl(0, 1%, 44%)";
-
-        result = currnetMonth-month;
+        hideError("month","monthLb");
+        result = currentMonth-month;
         return result;
     }
 }
 
-
 //Calculate day of age
 function dayCalculator(day){
-
     let birthMonth = parseInt(month.value);
-    let currnetDay = date.getDate();
+    let currentDay = date.getDate();
     let addedDay =0 ; 
     let result = 0;
 
-    //If currnet Day be less than birth day this block assignment the added day
-    if(currnetDay < day){
+    //If current Day be less than birth day this block assignment the added day
+    if(currentDay < day){
 
-        //Months of year have 30 days (to add currnet Day if day be greater)
+        //Months of year have 30 days (to add current Day if day be greater)
         if( birthMonth == 4 || birthMonth == 6 || birthMonth == 9 ||birthMonth == 11){
             addedDay = 30;
         }
@@ -124,32 +143,24 @@ function dayCalculator(day){
         //February month have 28 or 29 day(leap years)
         else if(birthMonth == 2 ){
             addedDay= year % 4 == 0 ? 29 : 28;
-        }
-        
+        } 
     }
 
     //Invalid date error
     if(day<1 || day>31){
-        document.getElementById("dayError").style.display="block";
-        document.getElementById("day").style.border="1px solid red";
-        document.getElementById("dayLb").style.color="red";
+        showError("day", "dayLb")
         return"--";
     }
 
     //This months have 30 days (if input be 31 its invalid)
-    else if((birthMonth == 4 || birthMonth == 6 || birthMonth == 9 ||birthMonth == 11)   &&  day == 31){
-        document.getElementById("dayError").style.display="block";
-        document.getElementById("day").style.border="1px solid red";
-        document.getElementById("dayLb").style.color="red";
+    else if((birthMonth == 4 || birthMonth == 6 || birthMonth == 9 || birthMonth == 11)   &&  day == 31){
+        showError("day", "dayLb");
         return"--";
     }
 
     else {
-        document.getElementById("dayError").style.display="none";
-        document.getElementById("day").style.border="solid 1px hsl(0, 1%, 44%)";
-        document.getElementById("dayLb").style.color=" hsl(0, 1%, 44%)";
-
-        result = (currnetDay + addedDay) - day;
+        hideError("day", "dayLb");
+        result = (currentDay + addedDay) - day;
         return result;
     }               
 }
